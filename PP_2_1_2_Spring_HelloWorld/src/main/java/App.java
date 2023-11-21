e547b298b2a30f6750b1d312d612811ee146b075
NbmanMol <125892325+NbmanMol@users.noreply.github.com>
2023-11-21 16:19:00 +0500

    Add files via upload

---
+++ PP_2_1_2_Spring_HelloWorld/src/main/java/App.java
@@ -0,0 +1,16 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat = (Cat) applicationContext.getBean("Cat");
+        Cat cat1 = (Cat) applicationContext.getBean("Cat");
+
+        System.out.println(bean == bean1);
+        System.out.println(cat1 == cat);
+    }
+}
