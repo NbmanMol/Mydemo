2f731e6989c38a348f1068a6dbe70fceab07ff7f
NbmanMol <125892325+NbmanMol@users.noreply.github.com>
2023-12-12 16:16:22 +0500

    Add files via upload

---
+++ PP_2_2_2_spring-mvc/src/main/java/web/controller/CarController.java
@@ -0,0 +1,28 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarService;
+
+@Controller
+public class CarController {
+    private final CarService carService;
+
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping("/cars")
+    public String printCarTable(@RequestParam(value = "count", required = false) Integer count, Model model) {
+        if (count == null || count >= 5) {
+            model.addAttribute("fixedCar", carService.getCarList());
+        } else {
+            model.addAttribute("fixedCar", carService.getFixedCarList(count));
+        }
+        return "cars";
+    }
+}
